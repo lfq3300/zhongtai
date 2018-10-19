afd5691b62bbfddb3edbf0837fada574278df0a0
lfq3300 <lfq3300@qq.com>
2018-10-19 17:41:56 +0800

    no message

--- phpinfo.php
+++ phpinfo.php
@@ -1,14 +1,3 @@
 <?php
-$shell = "ls -la";
-exec($shell, $result, $status);
-print_r($result);
-print_r($status);
-$shell = "<font color='red'>$shell</font>";
-echo "<pre>";
-if( $status ){
-    echo "shell命令{$shell}执行失败";
-} else {
-    echo "shell命令{$shell}成功执行, 结果如下<hr>";
-    print_r( $result );
-}
-echo "</pre>";
+$out02 = shell_exec('ls -l');
+print_r($out02);
