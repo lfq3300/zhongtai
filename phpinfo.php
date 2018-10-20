ee409743e980781d9bd584a584e0ff1f08342d3b
luo <luo3300@qq.com>
2018-10-20 14:12:35 +0800

    no message

--- phpinfo.php
+++ phpinfo.php
@@ -1,3 +1,8 @@
 <?php
-exec("mkdir demo");
-echo exec("git pull origin develop", $result);
+error_reporting(1);
+$token = 'ZyDbXzyro6Likbww';
+$json = json_decode(file_get_contents('php://input'), true);
+if (empty($json['token']) || $json['token'] !== $token) {
+    exit('error request');
+}
+echo exec('git pull origin develop');
