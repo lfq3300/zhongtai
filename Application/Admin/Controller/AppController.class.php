48ba16594f1792d24d425518acfb0b244b64d922
lfq3300 <lfq3300@qq.com>
2018-10-22 16:03:34 +0800

    no message

--- Application/Admin/Controller/AppController.class.php
+++ Application/Admin/Controller/AppController.class.php
@@ -13,21 +13,17 @@ use Think\Controller;
 
 class AppController extends Controller {
     public function index(){
-        $Auth = new AuthorizeController();
-        echo $Auth->acc();
+        $builder = new AdminListBuilder();
+        $builder
+            ->title("公众号列表")
+            ->powerAdd(U("add"))
+            ->display();
     }
-
-//    public function index(){
-//        $builder = new AdminListBuilder();
-//        $builder
-//            ->title("公众号列表")
-//            ->powerAdd(U("add"))
-//            ->display();
-//    }
 
     public function add(){
         $component_appid = C('ZTAPPID');
-        $pre_auth_code = $this->getPreAuthCode();
+        $Auth = new AuthorizeController();
+        $pre_auth_code = $Auth->getPreAuthCode();
         $this->assign('AuthorizeUrl', "https://mp.weixin.qq.com/cgi-bin/componentloginpage?component_appid=$component_appid&pre_auth_code=$pre_auth_code&redirect_uri=http://zt.ltthk.top/index.php/wx/AuthorizeCallback");
         $this->display();
     }
