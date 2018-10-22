60acb0663979d20c0d37659326d925a212b02767
lfq3300 <lfq3300@qq.com>
2018-10-22 08:44:02 +0800

    no message

---
+++ develop.php
@@ -0,0 +1,7 @@
+<?php
+//$token = 'ZyDbXzyro6Likbww';
+//$json = json_decode(file_get_contents('php://input'), true);
+//if (empty($json['token']) || $json['token'] !== $token) {
+//    exit('error request');
+//}
+echo exec('git pull origin develop');
--- phpinfo.php
+++
@@ -1,8 +0,0 @@
-<?php
-error_reporting(1);
-$token = 'ZyDbXzyro6Likbww';
-$json = json_decode(file_get_contents('php://input'), true);
-if (empty($json['token']) || $json['token'] !== $token) {
-    exit('error request');
-}
-echo exec('git pull origin develop');
