e75fea926c96274f1b124d86f74e1ada4bedefc2
lfq3300 <lfq3300@qq.com>
2018-10-23 10:42:59 +0800

    no message

--- Application/Admin/Controller/WxController.class.php
+++ Application/Admin/Controller/WxController.class.php
@@ -64,6 +64,7 @@ class WxController extends Controller{
             $appInfo = curl_get_https ($url1,json_encode($data1,true));
             $appInfo = json_decode($appInfo,true);
             $authorizer_info = $appInfo['authorizer_info'];
+            print_r($authorizer_info);
             //缓存公众号access_token
             S($authorizer_appid."access_token",$authorizer_access_token,7200);
             $appData = [
