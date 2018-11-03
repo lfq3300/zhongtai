8cb11fd1629c34c77336327d719b1da89b2ad0df
luo <luo3300@qq.com>
2018-11-03 10:37:55 +0800

    no message

--- Application/Admin/Controller/WxController.class.php
+++ Application/Admin/Controller/WxController.class.php
@@ -11,9 +11,9 @@ class WxController extends Controller
         echo "success";
     }
     public function savData(){
-        M()->query("UPDATE mc_app set synchron = 1");
-        M()->query("delete from mc_app_data");
-        M()->query("delete from mc_app_fans");
+        M()->execute("UPDATE mc_app set synchron = 1");
+        M()->execute("delete from mc_app_data");
+        M()->execute("delete from mc_app_fans");
     }
     /*
      * 每隔10分钟 微信回调并且更新 ticket  存入数据库
