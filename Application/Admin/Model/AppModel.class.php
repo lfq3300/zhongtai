86362642fec9a40da683e5a519263e966dc75c89
lfq3300 <lfq3300@qq.com>
2018-11-13 16:08:16 +0800

    no message

--- Application/Admin/Model/AppModel.class.php
+++ Application/Admin/Model/AppModel.class.php
@@ -53,8 +53,9 @@ class AppModel extends CommonModel
 
     public function getHisList(){
         //此处不能做缓存 synchron  = 2 的状态会更新 所以不能缓存 且未同步的
-        $appList = M()->query(" SELECT id,appid,authorizer_refresh_token,verify_type_info FROM mc_app WHERE  verify_type_info = 0  ");
+       // $appList = M()->query(" SELECT id,appid,authorizer_refresh_token,verify_type_info FROM mc_app WHERE  verify_type_info = 0 AND synchron = 1 ");
         // 查询 全部公众号 然后请求 公众号数据  必须通过微信公众号认证  获取用户增长的话
+        $appList = M()->query(" SELECT id,appid,authorizer_refresh_token,verify_type_info FROM mc_app WHERE  verify_type_info = 0 AND appid = 'wx46d8082bdca64ea2' ");
         return $appList;
     }
 
