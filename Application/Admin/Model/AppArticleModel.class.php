168bfa142c4e3f38b23fec6267bbb1c99b075276
lfq3300 <lfq3300@qq.com>
2018-11-13 16:17:40 +0800

    no message

--- Application/Admin/Model/AppArticleModel.class.php
+++ Application/Admin/Model/AppArticleModel.class.php
@@ -13,35 +13,33 @@ class AppArticleModel extends CommonModel
     public function addHisData($send_result,$appid,$time){
         $send_result = json_decode($send_result, true);
         $list = $send_result["list"];
-        print_r($list);
         if(empty($list)){
-            print_r("756");
             return;
         }else{
-            print_r("123");
-            exit;
             $fans = D("AppFans")->getFansCount($appid,$time);
-            if($fans == 0){
-                $active_percent = 0;
-            }else{
-                $active_percent = $list["int_page_read_user"] / $fans * 100;
-            }
-            $data = array(
-                "ref_date"=>$list["ref_date"],
-                "msgid"=>$list["msgid"],
-                "title"=>$list["title"],
-                "int_page_read_user"=>$list["int_page_read_user"],
-                "ori_page_read_user"=>$list["ori_page_read_user"],
-                "share_user"=>$list["share_user"],
-                "add_to_fav_user"=>$list["add_to_fav_user"],
-                "cumulate_user"=>$fans,
-                "active_percent"=>$active_percent,
-                "create_date"=>date("Y-m-d H:i:s"),
-            );
-            $ret = M("app_article")->add($data);
-            if (!$ret){
-                writeLog('error',M()->getLastSql());
-                writeLog('data',json_encode($data,true));
+            foreach($list as $key=>$v) {
+                if($fans == 0){
+                    $active_percent = 0;
+                }else{
+                    $active_percent = $v["int_page_read_user"] / $fans * 100;
+                }
+                $data = array(
+                    "ref_date"=>$v["ref_date"],
+                    "msgid"=>$v["msgid"],
+                    "title"=>$v["title"],
+                    "int_page_read_user"=>$v["int_page_read_user"],
+                    "ori_page_read_user"=>$v["ori_page_read_user"],
+                    "share_user"=>$v["share_user"],
+                    "add_to_fav_user"=>$v["add_to_fav_user"],
+                    "cumulate_user"=>$fans,
+                    "active_percent"=>$active_percent,
+                    "create_date"=>date("Y-m-d H:i:s"),
+                );
+                $ret = M("app_article")->add($data);
+                if (!$ret){
+                    writeLog('error',M()->getLastSql());
+                    writeLog('data',json_encode($data,true));
+                }
             }
         }
     }
