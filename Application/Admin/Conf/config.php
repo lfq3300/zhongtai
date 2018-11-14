54caf376e63991423e2450e6932852aa04b51c18
lfq3300 <lfq3300@qq.com>
2018-11-14 15:23:36 +0800

    no message

--- Application/Admin/Conf/config.php
+++ Application/Admin/Conf/config.php
@@ -4,7 +4,7 @@ return array(
     'DB_HOST'   => '127.0.0.1', // 服务器地址
     'DB_NAME'   => 'zt.ltthk.top', // 数据库名
     'DB_USER'   => 'root', // 用户名
-    'DB_PWD'    => 'root',  // 密码
+    'DB_PWD'    => '1166a340e1874590',  // 密码
     'DB_PORT'   => '3306', // 端口
     'DB_PREFIX' => 'mc_', // 数据库表前缀
 
